ed35848fa0ab3a1202cb3fef73ae1ffba29f84a8
Yogibear-pixel-99 <joachim.puercher@gmail.com>
2025-07-09 23:11:05 +0200

    added working animations and links to the mobile header menu

--- src/app/hero-section/header/header-links/header-links.component.ts
+++ src/app/hero-section/header/header-links/header-links.component.ts
@@ -11,4 +11,13 @@ import { CommonModule } from '@angular/common';
 export class HeaderLinksComponent {
 
 langService = inject(LanguageService);
+
+clickedLinkIndex: number | null = null;
+
+setLinkIndex(pos: number){
+  this.clickedLinkIndex = pos;
+  // setTimeout(() => navigate to site pos, 0.5);
+  setTimeout(() => this.clickedLinkIndex = null, 500);
+}
+
 }
