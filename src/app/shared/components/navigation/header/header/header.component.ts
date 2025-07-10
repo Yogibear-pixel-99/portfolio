41adee1e4d2ff23d455efd0ed55a4372f51c5594
Yogibear-pixel-99 <joachim.puercher@gmail.com>
2025-07-10 23:05:26 +0200

    added about me section and the first images for the left content

--- src/app/shared/components/navigation/header/header/header.component.ts
+++ src/app/shared/components/navigation/header/header/header.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { LanguageService } from '../../../../services/languages/language.service';
 import { MobileHeaderComponent } from '../mobile-header/mobile-header.component';
 import { LangSwitchComponent } from '../../../ui/lang-switch/lang-switch.component';
+import { HeaderLinksComponent } from '../header-links/header-links.component';
 
 @Component({
   standalone: true,
   selector: 'app-header',
-  imports: [CommonModule, MobileHeaderComponent, LangSwitchComponent],
+  imports: [CommonModule, MobileHeaderComponent, LangSwitchComponent, HeaderLinksComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
