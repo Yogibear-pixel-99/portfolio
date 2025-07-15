73d62cedccef809d177ef6b06126ca627f9b3f47
Yogibear-pixel-99 <joachim.puercher@gmail.com>
2025-07-15 18:33:02 +0200

    added text to about me section

--- src/app/about-me/about-me.component.ts
+++ src/app/about-me/about-me.component.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { TranslateService, TranslateModule } from '@ngx-translate/core';
+import { GetInContactButtonComponent } from '../shared/components/ui/get-in-contact-button/get-in-contact-button.component';
 
 @Component({
   selector: 'app-about-me',
-  imports: [],
+  imports: [TranslateModule, GetInContactButtonComponent],
   templateUrl: './about-me.component.html',
   styleUrl: './about-me.component.scss'
 })
 export class AboutMeComponent {
-
+translate = inject(TranslateService);
 }
