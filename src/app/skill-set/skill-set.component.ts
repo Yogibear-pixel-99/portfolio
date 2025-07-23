da3e39469af3221fdf1d0becf33b852f57fde019
Yogibear-pixel-99 <joachim.puercher@gmail.com>
2025-07-23 16:35:55 +0200

    added responsive skill section (excluding sticker) for all devices, added new font mixing for standard text, added same responsive padding to about me and skill section for all device sizes.

--- src/app/skill-set/skill-set.component.ts
+++ src/app/skill-set/skill-set.component.ts
@@ -12,4 +12,26 @@ import { TechlogoService } from '../shared/services/techlogo/techlogo.service';
 export class SkillSetComponent {
 translate = inject(TranslateService);
 techlogoService = inject(TechlogoService);
+
+
+activeSticker: number = 0;
+stickerLogo: string[] = [
+  'images/skills/sticker_close.png',
+  'images/skills/sticker_middle.png',
+  'images/skills/sticker_open.png'
+]
+get getStickerPath() {
+  return this.stickerLogo[this.activeSticker];
+}
+
+openSticker(){
+  this.activeSticker = 1;
+  setTimeout(() => this.activeSticker = 2, 200);
+}
+
+closeSticker(){
+  setTimeout(() => this.activeSticker = 1, 1000);
+  setTimeout(() => this.activeSticker = 0, 1200);
+}
+
 }
