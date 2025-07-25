d45bf644ec40df90abed88140c6066318fd403eb
Yogibear-pixel-99 <joachim.puercher@gmail.com>
2025-07-25 13:04:55 +0200

    added button to the projects hoover content

--- src/app/main/my-projects/my-projects.component.ts
+++ src/app/main/my-projects/my-projects.component.ts
@@ -1,10 +1,11 @@
 import { Component, inject } from '@angular/core';
 import { TranslateService, TranslateModule } from '@ngx-translate/core';
 import { ProjectComponent } from "./project/project.component";
+import { MainButtonComponent } from "../../shared/components/ui/main-button/main-button.component";
 
 @Component({
   selector: 'app-my-projects',
-  imports: [TranslateModule, ProjectComponent],
+  imports: [TranslateModule, ProjectComponent, MainButtonComponent],
   templateUrl: './my-projects.component.html',
   styleUrl: './my-projects.component.scss'
 })
