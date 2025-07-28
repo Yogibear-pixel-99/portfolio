65ab2c1319df416b206a77a5e01ffdde2f0eab70
Yogibear-pixel-99 <joachim.puercher@gmail.com>
2025-07-28 19:48:53 +0200

    learning much about checkbox design, designed the form element

--- src/app/main/contact-me/contact-me.component.ts
+++ src/app/main/contact-me/contact-me.component.ts
@@ -1,14 +1,22 @@
+
 import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-contact-me',
-  imports: [TranslateModule],
+  imports: [TranslateModule, FormsModule],
   templateUrl: './contact-me.component.html',
   styleUrl: './contact-me.component.scss'
 })
 export class ContactMeComponent {
 
   translate = inject(TranslateService);
+
+  testCheck = false;
+
+  debuggTest(){
+    console.log(this.testCheck);
+  }
 
 }
