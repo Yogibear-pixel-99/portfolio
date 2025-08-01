6116dbdd5361da1cae9dc41726d4365039e04efa
Yogibear-pixel-99 <joachim.puercher@gmail.com>
2025-08-01 20:32:57 +0200

    added sticker component including responsive to the project details sites

--- src/app/main/my-projects/project-detail/right-content/right-content.component.ts
+++ src/app/main/my-projects/project-detail/right-content/right-content.component.ts
@@ -1,10 +1,11 @@
 import { Component, inject } from '@angular/core';
 import { ProjectDetailsService } from '../../../../shared/services/project-details/project-details.service';
 import { MainButtonComponent } from '../../../../shared/components/ui/main-button/main-button.component';
+import { MyLogoStickerComponent } from '../../../../shared/components/ui/my-logo-sticker/my-logo-sticker.component';
 
 @Component({
   selector: 'app-right-content',
-  imports: [MainButtonComponent],
+  imports: [MainButtonComponent, MyLogoStickerComponent],
   templateUrl: './right-content.component.html',
   styleUrl: './right-content.component.scss'
 })
