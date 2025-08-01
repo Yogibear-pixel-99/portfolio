eb5239b13a47c3295d304a0d23a65691e15f9682
Yogibear-pixel-99 <joachim.puercher@gmail.com>
2025-08-01 20:10:14 +0200

    finished creating and designing sticker component

--- src/app/main/contact-me/contact-me.component.ts
+++ src/app/main/contact-me/contact-me.component.ts
@@ -3,10 +3,11 @@ import { Component, inject } from '@angular/core';
 import { TranslateModule, TranslateService } from '@ngx-translate/core';
 import { FooterComponent } from "../../shared/components/footer/footer/footer.component";
 import { ContactMeFormComponent } from './contact-me-form/contact-me-form.component';
+import { MyLogoStickerComponent } from "../../shared/components/ui/my-logo-sticker/my-logo-sticker.component";
 
 @Component({
   selector: 'app-contact-me',
-  imports: [TranslateModule, FooterComponent, ContactMeFormComponent],
+  imports: [TranslateModule, FooterComponent, ContactMeFormComponent, MyLogoStickerComponent, MyLogoStickerComponent],
   templateUrl: './contact-me.component.html',
   styleUrl: './contact-me.component.scss'
 })
