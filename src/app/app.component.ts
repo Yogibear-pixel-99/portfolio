f01cf86e879957e7761cb9121771f3eeae8abebd
Yogibear-pixel-99 <joachim.puercher@gmail.com>
2025-08-20 10:56:22 +0200

    added scrolling animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,29 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MaskOverlayComponent } from './shared/components/mask-overlay/mask-overlay.component';
 import { TranslateService } from '@ngx-translate/core';
+import AOS from 'aos';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, MaskOverlayComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit, AfterViewInit{
   constructor(private translate: TranslateService) {
     this.translate.addLangs(['de', 'en']);
     this.translate.setDefaultLang('en');
     this.translate.use('en');
+  }
+    ngOnInit() {
+    AOS.init({
+      duration: 1000,
+      once: false,
+    });
+  }
+
+  ngAfterViewInit(){
+    AOS.refresh();
   }
   title = 'portfolio';
 }
