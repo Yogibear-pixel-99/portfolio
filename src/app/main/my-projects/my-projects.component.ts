45d788d0373f7f1327af4fc1ebf808948a966b5b
Yogibear-pixel-99 <joachim.puercher@gmail.com>
2025-08-23 18:02:05 +0200

    finished version after the levy

--- src/app/main/my-projects/my-projects.component.ts
+++ src/app/main/my-projects/my-projects.component.ts
@@ -1,8 +1,7 @@
-import { Component, inject, afterNextRender } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { TranslateService, TranslateModule } from '@ngx-translate/core';
 import { ProjectOverviewComponent } from './project-overview/project-overview.component';
 import { ProjectLinkButtonComponent } from '../../shared/components/ui/project-link-button/project-link-button.component';
-import { ViewportScroller } from '@angular/common';
 
 @Component({
   selector: 'app-my-projects',
